7c0d0e8b1b25da5d016da64d661c63a574709f5c
Luxofox <Luxofox93@gmail.com>
2015-03-10 15:06:28 -0430

    Creacion de clase Role y Creacion de las 2 tablas

--- FormsProyecto1/FormsProyecto1/MainMenu.vb
+++ FormsProyecto1/FormsProyecto1/MainMenu.vb
@@ -16,21 +16,88 @@ Public Class MainMenu
     Dim strConexion As String = "Data Source=(localdb)\ProjectsV12;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"
     Dim connection As New SqlConnection(strConexion)
     Dim dataTableUser As New DataTable
-    Public Function createDataTable() As DataTable
+    Dim dataTableRole As New DataTable
+    Public Function createDataTableUser() As DataTable
 
         dataTableUser = New DataTable
-        dataTableUser.Columns.Add("Sigla")
-        dataTableUser.Columns.Add("Nombre")
-        dataTableUser.Columns.Add("Nivel")
-        dataTableUser.Columns.Add("Créditos")
-        dataTableUser.Columns.Add("Capacidad")
-        dataTableUser.Columns.Add("Profesor")
+        dataTableUser.Columns.Add("User ID")
+        dataTableUser.Columns.Add("Name")
+        dataTableUser.Columns.Add("Last Name")
+        dataTableUser.Columns.Add("Username")
+        dataTableUser.Columns.Add("Password")
+        dataTableUser.Columns.Add("Active")
+        dataTableUser.Columns.Add("Entry Date")
+        dataTableUser.Columns.Add("Role ID")
 
-        createDataTable = dataTableUser
+        createDataTableUser = dataTableUser
+
+    End Function
+    Public Function askUserInputs() As User
+        Dim nameOfUser As String
+        Dim userName As String
+        Dim userId As Integer
+        Dim userLastname As String
+        Dim userStatus As Boolean
+        ' Dim userEntryDate As Date
+        Dim userPassword As String
+        Dim UserIdRole As Integer
+        Dim user As New User
+        nameOfUser = InputBox("Please enter the name of the user: ")
+        userLastname = InputBox("Please enter the last name of the user : ")
+        userName = InputBox("Please enter the username: ")
+        userPassword = InputBox("Please enter the password of the user: ")
+        UserIdRole = CInt(InputBox("Please enter the user Role: "))
+        userId = CInt(InputBox("Please enter the user ID: "))
+        userStatus = MsgBox("Please select if the user its active or not", MsgBoxStyle.YesNo, "Please select one")
+        If userStatus = 0 Then
+            userStatus = True
+        Else : userStatus = False
+        End If
+        'userEntryDate = InputBox()
+        'Here we create the object
+        user.user_Id = userId
+        user.user_Name = userName
+        user.name_Of_User = nameOfUser
+        user.user_Lastname = userLastname
+        user.user_Status = userStatus
+        user.user_Password = userPassword
+        user.User_IdRole = UserIdRole
+        'return user
+        askUserInputs = user
 
     End Function
 
+    Public Function createDataTableRole() As DataTable
 
+        dataTableUser = New DataTable
+        dataTableUser.Columns.Add("Role ID")
+        dataTableUser.Columns.Add("Role Name")
+        dataTableUser.Columns.Add("Role Description")
+       
+
+        createDataTableRole = dataTableUser
+
+    End Function
+    Public Function askRoleInputs() As Role
+        Dim roleId As Integer
+        Dim roleName As String
+        Dim roleDescription As String
+
+
+        Dim role As New Role
+        roleName = InputBox("Please enter the name of the user: ")
+        roleDescription = InputBox("Please enter the last name of the user : ")
+        roleId = CInt(InputBox("Please enter the user Role: "))
+
+
+        'Here we create the object
+        role.role_Id = roleId
+        role.role_Name = roleName
+        role.role_Description = roleDescription
+        'return course
+        askRoleInputs = role
+
+    End Function
 
     Private Sub MainMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
